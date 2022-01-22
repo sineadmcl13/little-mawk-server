0f2170bdc27364d1392065304f386e56932cd2f9
sineadmcl13 <sinead.mcl13@gmail.com>
2022-01-22 12:41:26 +0000

    Added first tests

---
+++ tests/services/rulesValidator.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const expect = chai.expect;
+const rulesValidator = require('../../services/rulesValidator');
+const testInputValues = require('./rulesValidatorTestInputs')
+
+describe("rulesValidator", () => {
+  context("isRuleSetValid", () => {
+    
+    context("valid test inputs", () => {
+      const tests = [
+        { description: "should pass for single rule", input: testInputValues.testInputSingleRule },
+        { description: "should pass for rule using 'all'", input: testInputValues.testInputSingleRuleWithAll },
+        { description: "should pass for rule using 'any", input: testInputValues.testInputSingleRuleWithAny },
+        { description: "should pass for multiple rules", input: testInputValues.testInputMultipleRuleWithAny}
+      ];
+    
+      tests.forEach(({description, input}) => {
+        it(`${description}`, ()=> {
+          expect(rulesValidator.isRuleSetValid(input)).to.be.true;
+        });
+      });
+    })
+
+    context("invalid test inputs", ()=>{
+      it("should fail for invalid json format input", ()=> {
+        expect(rulesValidator.isRuleSetValid("{\"someField\": \"noClosingBracket\"")).to.be.false;
+      });
+  
+      it("should fail for empty valid json input", ()=> {
+        expect(rulesValidator.isRuleSetValid("{}")).to.be.false;
+      });
+  
+      it("should fail if multiple 'any' blocks use same compare value");
+      it("should fail if 'compare' field is not a valid type");
+      it("should fail if 'operator' field is not a valid type");
+      it("should fail if 'response' object does not contain required fields");
+
+    })    
+  })
+});
---
+++ tests/services/rulesValidatorTestInputs.js
@@ -0,0 +1,119 @@
+exports.testInputSingleRule = {
+  rules: [
+    {
+      ruleName: "rule1",
+      request: {
+        any: [
+          {
+            compare: "endpoint",
+            value: "/no",
+            operator: "equal"
+          }
+        ]
+      },
+      response: {
+        code: 400,
+        body: "Server error response"
+      }
+    }
+  ]
+}
+
+exports.testInputSingleRuleWithAll = {
+  rules: [
+    {
+      ruleName: "rule1",
+      request: {
+        all: [
+          {
+            compare: "endpoint",
+            value: "/no",
+            operator: "equal"
+          },
+          {
+            compare: "endpoint",
+            value: "/fail",
+            operator: "equal"
+          }
+        ]
+      },
+      response: {
+        code: 400,
+        body: "Server error response"
+      }
+    },
+  ]
+}
+
+exports.testInputSingleRuleWithAny = {
+  rules: [
+    {
+      ruleName: "rule1",
+      request: {
+        any: [
+          {
+            compare: "endpoint",
+            value: "/no",
+            operator: "equal"
+          },
+          {
+            compare: "endpoint",
+            value: "/fail",
+            operator: "equal"
+          }
+        ]
+      },
+      response: {
+        code: 400,
+        body: "Server error response"
+      }
+    }
+  ]
+}
+
+exports.testInputMultipleRuleWithAny = {
+  rules: [
+    {
+      ruleName: "rule1",
+      request: {
+        any: [
+          {
+            compare: "endpoint",
+            value: "/no",
+            operator: "equal"
+          },
+          {
+            compare: "endpoint",
+            value: "/fail",
+            operator: "equal"
+          }
+        ]
+      },
+      response: {
+        code: 400,
+        body: "Server error response"
+      }
+    },
+    {
+      ruleName: "rule2",
+      request: {
+        any: [
+          {
+            compare: "endpoint",
+            value: "/test",
+            operator: "equal"
+          },
+          {
+            compare: "endpoint",
+            value: "/test2",
+            operator: "equal"
+          }
+        ]
+      },
+      response: {
+        code: 200,
+        body: "Server response ok"
+      }
+    }
+  ]
+}
