f733480c81b0c8a1e7ca74c02ab23068bf3d5643
sineadmcl13 <sinead.mcl13@gmail.com>
2022-03-06 11:41:29 +0000

    Dont write logs to file during tests

--- config/logging.js
+++ config/logging.js
@@ -5,23 +5,35 @@ const customFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp}: ${level}: ${message}`;
 });
 
+
+const consoleTransport = new transports.Console({
+  format: combine(
+    colorize(),
+    simple()
+  )
+});
+
+const fileTransport = new transports.File({ 
+  filename: './.logs/littlemawk.log', 
+  format: combine(
+    timestamp(),
+    customFormat
+  )
+});
+
+let configuredTransports = [
+  fileTransport,
+  consoleTransport
+]
+
+if(process.env.TEST){
+  //Don't write to file when running tests
+  configuredTransports = [ consoleTransport ]
+}
+
 const logger = createLogger({
   level: 'debug',
-  transports: [
-    new transports.File({ 
-      filename: './.logs/littlemawk.log', 
-      format: combine(
-        timestamp(),
-        customFormat
-      )
-    }),
-    new transports.Console({
-      format: combine(
-        colorize(),
-        simple()
-      )
-    })
-  ],
+  transports: configuredTransports,
   exitOnError: false
 });
 module.exports = logger;
