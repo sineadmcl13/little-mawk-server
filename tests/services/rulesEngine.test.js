6d819c94c4e0bd0c34b027655a45aa716c38c600
sineadmcl13 <sinead.mcl13@gmail.com>
2022-05-20 16:26:55 +0100

    Update tests in rulesEngine

--- tests/services/rulesEngine.test.js
+++ tests/services/rulesEngine.test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 const sinon = require('sinon')
 const validator = require('../../services/rulesValidator')
 const rulesEngine = require('../../services/rulesEngine');
+const testInputValues = require('./rulesEngineTestInputs')
 
 describe("rulesEngine", () => {
   context("addDefaultErrorRule", () => {
@@ -25,4 +26,29 @@ describe("rulesEngine", () => {
     });
 
   });
+
+  context("addParsedRules", () => {
+    it("should fall back to default rules if none provided", () => {
+
+      let requestRules = rulesEngine.build();
+      expect(requestRules.rules.length).to.equal(1)
+      
+      let defaultRule = requestRules.rules.pop();
+      expect(defaultRule.name).to.equal('defaultRule')
+      expect(defaultRule.ruleEvent.params.code).to.equal(200)
+      expect(defaultRule.ruleEvent.type).to.equal('response')
+    });
+    
+    it("should correctly add validated rules to rulesEngine", () => {
+      let requestRules = rulesEngine.build(testInputValues.testInputSingleRule);
+      expect(requestRules.rules.length).to.equal(1)
+      
+      let defaultRule = requestRules.rules.pop();
+      expect(defaultRule.name).to.equal('rule1')
+      expect(defaultRule.ruleEvent.params.code).to.equal(202)
+      expect(defaultRule.ruleEvent.params.body).to.equal('Test server response')
+      expect(defaultRule.ruleEvent.type).to.equal('response')
+    })
+  })
+
 });
